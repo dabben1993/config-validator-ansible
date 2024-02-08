f302b42f7cf0500261a809d0c36ddd643f9a4596
dabben1993 <gickis1993@live.com>
2024-02-08 10:21:32 +0100

    implemented the file_helper methods into file_validator to make it simpler

--- scripts/file_validator.py
+++ scripts/file_validator.py
@@ -4,7 +4,6 @@ import os
 from structlog import get_logger
 import cerberus
 import yaml
-import file_helper
 
 log = get_logger()
 
@@ -26,16 +25,35 @@ class FileValidator:
             return cerberus.Validator(schema)
 
     def process_configuration(self):
-        config_data = file_helper.read_yaml(self.file_path)
+        config_data = self.read_yaml()
         if config_data:
             log.info("Config data exists", config_data=config_data)
             if self.validator.validate(config_data):
                 log.info("Configuration is valid")
-                file_helper.save_as_json(data=json.dumps(config_data, indent=2),
-                                         file_name=os.path.splitext(os.path.basename(self.file_path))[0],
-                                         output_directory=self.destination)
+                self.save_as_json(data=json.dumps(config_data, indent=2),
+                                  file_name=os.path.splitext(os.path.basename(self.file_path)))
+
                 log.info("File saved")
 
             else:
                 for error in self.validator.errors:
                     log.warning("Invalid configuration:", f"  - {error}: {self.validator.errors[error]}")
+
+    def save_as_json(self, data, file_name):
+        try:
+            os.makedirs(self.destination, exist_ok=True)
+            json_output_path = os.path.join(self.destination, file_name + ".json")
+            with open(json_output_path, 'w') as json_output_file:
+                json.dump(data, json_output_file, indent=2)
+            print("saved")
+            log.info("File saved as JSON", data=data, file_path=json_output_path)
+        except Exception as e:
+            log.error("Error saving JSON file", error=str(e))
+
+    def read_yaml(self):
+        try:
+            with open(self.file_path, 'r') as yaml_file:
+                return yaml.safe_load(yaml_file)
+        except Exception as e:
+            log.error("Error reading YAML file", error=str(e))
+            return None
